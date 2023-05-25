75f35b0397306f8ba929f60a4411306577826368
Isagani-lapira <lapiraisagani@gmail.com>
2023-05-25 10:55:18 +0800

    Finish game functionality

--- Assets/Scripts/ballScript.cs
+++ Assets/Scripts/ballScript.cs
@@ -1,21 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ballScript : MonoBehaviour
 {
     public int speed;
+    private int yAxis, player1_score, player2_score;
+    private bool goingRight, goingUp;
+    public Text scoreBoard;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        yAxis = 0; //as default
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position -= new Vector3(1, 0, 0)*(speed * Time.deltaTime);
+        //movement of the ball
+        if (goingRight)
+            transform.position -= new Vector3(1, yAxis, 0) * (speed * Time.deltaTime);
+        else
+            transform.position += new Vector3(1, yAxis, 0) * (speed * Time.deltaTime);
     }
 
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //make the ball goes randomly upward or downward
+        int random = Random.Range(-1, 1);
+        yAxis = random;
+
+
+        //after making collision to paddle ball will go to the opposite site like bouncing effect
+        if (collision.gameObject.tag == "paddles")
+        {
+            if (goingRight)
+                goingRight = false;
+            else
+                goingRight = true;
+                
+        }
+        else
+        {
+            //change the y-axis if the ball goes to the wall
+            if (goingUp)
+            {
+                goingUp = false;
+                yAxis = -1;
+            }
+
+            else
+            {
+                yAxis = 1;
+                goingUp = true;
+            }
+                
+        }
+
+        speed++;
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //check who need to give the point
+        if (collision.gameObject.name == "Leftside")
+            player2_score++;
+        else
+            player1_score++;
+
+        
+        scoreBoard.text = player1_score+" : "+player2_score; //change the score 
+        transform.position = new Vector3(0, 0 , 0); //like a restart way
+        speed = 5; //restart the speed to 5
+    }
 
 }
--- Assets/Scripts/paddle.cs
+++ Assets/Scripts/paddle.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,28 @@ using UnityEngine;
 public class paddle : MonoBehaviour
 {
 
-    public GameObject player;
-    private void OnCollisionEnter2D(Collision2D collision)
+    public GameObject paddle1,paddle2;
+
+    public void Update()
     {
-        Debug.Log(gameObject.name);
-        Debug.Log(player.name);
+        if (Input.GetKey(KeyCode.UpArrow))
+            movement(paddle2, true);
+        if(Input.GetKey(KeyCode.DownArrow))
+            movement(paddle2,false);
+        if(Input.GetKey(KeyCode.W))
+            movement(paddle1, true);
+        if (Input.GetKey(KeyCode.S))
+            movement(paddle1, false);
+
     }
+
+    private void movement(GameObject paddle, bool isUp)
+    {
+        if(isUp)
+            paddle.transform.position += new Vector3(0, 1, 0) * (3 * Time.deltaTime);
+        else
+            paddle.transform.position -= new Vector3(0, 1, 0) * (3 * Time.deltaTime);
+    }
+
+
 }
